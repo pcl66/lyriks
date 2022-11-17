7dd6f1662506c49bbfffaa12fd8803acb4ec90db
pcl <1500371773@qq.com>
2022-11-18 01:16:05 +0800

    feat: Discover some styles

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,15 @@
-const Discover = () => <div>Discover</div>;
+const Discover = () => {
+  console.log('xxx');
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+        <select>
+          {[1, 2, 3, 4].map((v) => (<option>123</option>))}
+        </select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
