ed9c214c295540c5bab0e692a30b9090f14c2105
clp <1500371773@qq.com>
2022-11-21 00:18:56 +0800

    XX

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
 
   return (
     <div className="relative flex">
-      {/* <Sidebar /> */}
+      <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
 
